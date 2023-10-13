49486739a39ef43c0ceda9e3ddebe490efe26936
Kirk Alesna <ALESNA@MLHUILLIER.LOCAL>
2023-10-13 09:15:24 +0800

    first commit

---
+++ src/Pages/Sendout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Sendout = (props: Props) => {
+  return (
+    <div className="mt-20">
+      <h1>Welcome to Sendout!</h1>
+    </div>
+  );
+};
+
+export default Sendout;
